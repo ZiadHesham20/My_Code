f31be74f2e15c313809e8bf7afa75caf4df54c4b
Ziad Hesham <zizohesham20@gmail.com>
2024-06-22 15:23:26 +0300

    logo done

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -34,12 +34,12 @@ function Navbar() {
                   <span className="absolute -inset-1.5"></span>
                   <span className="sr-only">Open user menu</span>
                   <img
-                    className="h-8 w-8 rounded-full"
-                    src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                    className="h-8 w-8 rounded-full object-contain"
+                    src="websiteLogo.png"
                     alt=""
                   />
                 </button>
-                <p className="text-white ms-2">Name</p>
+                <p className="text-white ms-2">Hassan Abu Ali</p>
               </div>
 
               {/* Dropdown menu */}
